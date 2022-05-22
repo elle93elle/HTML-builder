c91e0ddd265c52d17f46ccedd68bf36a8f64cd6e
Elina <elle93elle@gmail.com>
2022-05-22 22:40:24 +0500

    fix: deleted file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,29 +1,25 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 const { stdin, stdout } = process;
 
-fs.writeFile( path.join(__dirname, 'text.txt'),'',(error) => {
-    if (error) return console.error(error.message);
-  })
+fs.writeFile(path.join(__dirname, "text.txt"), "", (error) => {
+  if (error) return console.error(error.message);
+});
 
-stdout.write('Введите ваш текст \n');
+stdout.write("Введите ваш текст \n");
 
-    stdin.on('data', data => {
-        if (data.toString().trim() === 'exit') {
-            stdout.write('До встречи \n');
-            process.exit();
-        }
+stdin.on("data", (data) => {
+  if (data.toString().trim() === "exit") {
+    stdout.write("До встречи \n");
+    process.exit();
+  }
 
-        fs.appendFile(
-            path.join(__dirname, 'text.txt'),
-            data,
-            err => {
-              if (err) throw err;
-            }
-          ); 
-    });
+  fs.appendFile(path.join(__dirname, "text.txt"), data, (err) => {
+    if (err) throw err;
+  });
+});
 
-    process.on('SIGINT', () => {
-        stdout.write('До встречи! \n');
-        process.exit();
-    });
+process.on("SIGINT", () => {
+  stdout.write("До встречи! \n");
+  process.exit();
+});
