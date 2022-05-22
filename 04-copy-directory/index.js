918437a18a7b745ce354ecd1812af2fa496ae07f
Elina <elle93elle@gmail.com>
2022-05-22 18:10:30 +0500

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,15 @@
+const fs = require('fs');
+const path = require('path');
 
+fs.mkdir( path.join(__dirname, 'files-copy'),(error) => {
+    if (error) return console.error(error.message);
+  })
+
+  fs.readdir(path.join(__dirname,'files'), (error, files) => {
+    if (error) return console.error(error.message);
+    for (let file of files) {
+        fs.copyFile(path.join(__dirname,'files', file),path.join(__dirname,'files-copy',file), error => {
+            if (error) return console.error(error.message);
+        })
+    }
+})
